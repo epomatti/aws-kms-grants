79b36f889cfea4a3a50ab30daddebf695b436a27
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-09-03 23:03:15 -0300

    bucket key

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -4,20 +4,23 @@ resource "aws_s3_bucket" "main" {
   force_destroy = true
 }
 
-# resource "aws_s3_bucket_versioning" "main" {
-#   bucket = aws_s3_bucket.main.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
-
 resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
   bucket = aws_s3_bucket.main.id
 
   rule {
+    bucket_key_enabled = true
+
     apply_server_side_encryption_by_default {
       kms_master_key_id = var.kms_key_arn
       sse_algorithm     = "aws:kms"
     }
+  }
+}
+
+# Not required, just to remove warnings
+resource "aws_s3_bucket_versioning" "main" {
+  bucket = aws_s3_bucket.main.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
