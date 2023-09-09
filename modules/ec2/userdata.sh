0683719f01a8cf659dcef578170dca37083b911e
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-09-09 17:00:42 -0300

    cloud-init

--- modules/ec2/userdata.sh
+++ modules/ec2/userdata.sh
@@ -6,6 +6,7 @@ export NEEDRESTART_MODE=a
 
 sudo apt update
 sudo apt upgrade -y
+sudo apt install unzip -y
 
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
